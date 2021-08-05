4783e79ceb99c95a610568101066f8418b761eb6
yigituygun8 <87874300+yigituygun8@users.noreply.github.com>
2021-08-05 18:33:36 +0300

    Update Day, Week, Month, Year Algorithm.py

--- Day, Week, Month, Year Algorithm.py
+++ Day, Week, Month, Year Algorithm.py
@@ -2,28 +2,37 @@
 # also, increase month variable every 30 days, week variable every 7 days and day variable every 1 day
 
 
-day = 0
-week = 0 
-month = 0
-year = 0
 
-# 1 year = 365 days || 1 month = 30 days || 1 week = 7 days 
+import time
 
-howmany = int(input("How Many Days Do You Want?: "))
+print("Counting the days... (Press Ctrl+C to terminate)")
 
-for i in range(howmany):
-    day += 1
+try:
+    day = 0
+    week = 0
+    month = 0
+    year = 0
+    while(True):
+        for i in range(365):
+            day += 1
+            time.sleep(0.3)
+            if day % 7 == 0:
+                week += 1
+                time.sleep(0.3)
+            if day % 30 == 0:
+                month += 1
+                time.sleep(0.3)
+            if day % 365 == 0:
+                year += 1
+                time.sleep(0.3)
+        if i == 364:
+            print("Day:", day, "\n"+"Week:", week, "\n" + "Month:", month, "\n"+"Year:", year)
+            break
 
-for j in range(day // 7):
-    week += 1
 
-for k in range(day // 30):
-    month += 1
-
-for n in range(day // 365):
-    year += 1
-
-print("Day:",day,"\n"+"Week:",week,"\n"+"Month:",month,"\n"+"Year:",year)
+except KeyboardInterrupt:
+    print("Day:", day, "\n"+"Week:", week, "\n" + "Month:", month, "\n"+"Year:", year)
+    pass
 
 
 
